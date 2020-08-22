cc4631d10e47d5e3f86ad4090c361d9d1dab8969
MLRichter <matsrichter@gmail.com>
2020-08-22 21:28:59 +0200

    build metrics

---
+++ phd_lab/metrics/classification.py
@@ -0,0 +1,61 @@
+from attr import attrs
+from torch import Tensor
+from typing import Union, Tuple
+import torch
+
+
+@attrs(auto_attribs=True, slots=True)
+class Top5Accuracy:
+
+    total: Union[int, float, Tensor] = 0
+    correct: Union[int, float, Tensor] = 0
+    name: str = "top5-accuracy"
+
+    def _accuracy(self, output: Tensor, target: Tensor, topk: Tuple[int] = (1,)):
+        """Computes the precision@k for the specified values of k"""
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred).long())
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0)
+            res.append(correct_k.mul_(100.0 / batch_size).item())
+        return res
+
+    @property
+    def value(self) -> float:
+        return self.correct / self.total
+
+    def update(self, y_true: Tensor, y_pred: Tensor) -> None:
+        _, predicted = torch.max(y_pred.data, 1)
+        self.correct += self._accuracy(y_pred, y_true, (5,))[0]
+        self.total += y_true.size(0)
+
+    def reset(self) -> None:
+        self.total = 0
+        self.correct = 0
+
+
+@attrs(auto_attribs=True, slots=True)
+class Accuracy:
+
+    total: Union[int, float, Tensor] = 0
+    correct: Union[int, float, Tensor] = 0
+    name: str = "accuracy"
+
+    @property
+    def value(self) -> float:
+        return self.correct / self.total
+
+    def update(self, y_true: Tensor, y_pred: Tensor) -> None:
+        _, predicted = torch.max(y_pred.data, 1)
+        self.correct += (predicted == y_true.long()).sum().item()
+        self.total += y_true.size(0)
+
+    def reset(self) -> None:
+        self.total = 0
+        self.correct = 0
