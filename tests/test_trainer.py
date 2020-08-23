2bebbe12df16a3a3bc04eefdaeadc8506352a6c7
MLRichter <matsrichter@gmail.com>
2020-08-23 22:20:31 +0200

    added first tests for trainer

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -3,20 +3,21 @@ import os
 from shutil import rmtree
 from phd_lab.experiments.trainer import Trainer
 from phd_lab.metrics import Accuracy, Top5Accuracy
-from phd_lab.models import vgg13
+from phd_lab.models import vgg11
 from phd_lab.datasets.cifar import Cifar10
 from phd_lab.optimizers import adam
 import torch
 
+
 @pytest.fixture()
 def trainer():
-    model = vgg13(num_classes=10)
+    model = vgg11(num_classes=10)
     return Trainer(
         model=model,
-        data_bundle=Cifar10(batch_size=64, output_size=32, cache_dir='tmp'),
+        data_bundle=Cifar10(batch_size=128, output_size=32, cache_dir='tmp'),
         optimizer_bundle=adam(model=model),
         run_id="test",
-        batch_size=64,
+        batch_size=128,
         epochs=1,
         metrics=[Accuracy(), Top5Accuracy()],
         logs_dir='./test_logs/',
@@ -31,12 +32,16 @@ class TestInit:
         rmtree('./test_logs')
 
     def test_folder_structure_deep_correct(self, trainer):
-        assert os.path.exists('./test_logs/VGG13/Cifar10_32/test')
+        assert os.path.exists('./test_logs/VGG11/Cifar10_32/test')
         rmtree('./test_logs/')
 
 
 class TestTrain:
 
     def test_training(self, trainer):
+        trainer.train()
+        assert os.path.exists(trainer._save_path)
+
+    def test_resume_training(self, trainer):
         trainer.train()
         assert os.path.exists(trainer._save_path)
