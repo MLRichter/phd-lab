3468dd5f6f81c12bba5e5afaf33010c0ecc22185
MLRichter <matrichter@uos.de>
2021-03-07 12:31:32 +0100

    updated resnet24

--- phd_lab/models/resnet.py
+++ phd_lab/models/resnet.py
@@ -1187,6 +1187,31 @@ def resnet18_dspl8(pretrained=False, **kwargs):
     model.name = 'ResNet18_DSPL8'
     return model
 
+def resnet36_dspl8(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2, 2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    model.name = 'ResNet36_DSPL8'
+    return model
+
+
+def resnet36(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [4, 4, 4, 4], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    model.name = 'ResNet36_DSPL8'
+    return model
+
 
 def resnet18_l05_w05(pretrained=False, **kwargs):
     """Constructs a ResNet-18 model.
