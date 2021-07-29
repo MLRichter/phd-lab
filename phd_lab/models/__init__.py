aa4333578fcb2527edd1e8dc9f01b5398b746f9b
MLRichter <matrichter@uos.de>
2021-07-29 07:58:32 +0200

    ready for chapter X experiments

--- phd_lab/models/__init__.py
+++ phd_lab/models/__init__.py
@@ -13,10 +13,10 @@ from .resnetModified import \
     PC_ResNet18_XS as ResNet18_PC_XS, \
     PC_ResNet18_XXS as ResNet18_PC_XXS, \
     PC_ResNet18_XXXS as ResNet18_PC_XXXS, \
-    PC_ResNet34_S as ResNet18_PC_XS, \
-    PC_ResNet34_XS as ResNet18_PC_XXS, \
-    PC_ResNet34_XXS as ResNet18_PC_XXS, \
-    PC_ResNet34_XXXS as ResNet18_PC_XXXS, \
+    PC_ResNet34_S as ResNet34_PC_XS, \
+    PC_ResNet34_XS as ResNet34_PC_XXS, \
+    PC_ResNet34_XXS as ResNet34_PC_XXS, \
+    PC_ResNet34_XXXS as ResNet34_PC_XXXS, \
     PC_ResNet18T, \
     PC_ResNet34T
 from .msnet import msnet18, msnet18nt, msnet18_ntns, msnet18_ns
