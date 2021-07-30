1881ae1a0241076528089ec18a8598e4ed6eefc1
MLRichter <matrichter@uos.de>
2021-07-30 16:53:24 +0200

    ready for chapter X experiments

--- phd_lab/models/transformers.py
+++ phd_lab/models/transformers.py
@@ -20,6 +20,12 @@ def vit_6_128(num_classes=10, img_size=256, **kwargs):
     return model
 
 
+def vit_12_128(num_classes=10, img_size=256, **kwargs):
+    model = ViT(img_dim=img_size, in_channels=3, patch_dim=img_size//16, num_classes=num_classes, dim=128, blocks=12)
+    model.name = "ViT_12_128"
+    return model
+
+
 def vit_6_256(num_classes=10, img_size=256, **kwargs):
     model = ViT(img_dim=img_size, in_channels=3, patch_dim=img_size//16, num_classes=num_classes, dim=256)
     model.name = "ViT_6_256"
@@ -28,7 +34,7 @@ def vit_6_256(num_classes=10, img_size=256, **kwargs):
 
 def vit_3_256(num_classes=10, img_size=256, **kwargs):
     model = ViT(img_dim=img_size, in_channels=3, patch_dim=img_size//16, num_classes=num_classes, dim=256)
-    model.name = "ViT"
+    model.name = "ViT_3_256"
     return model
 
 
