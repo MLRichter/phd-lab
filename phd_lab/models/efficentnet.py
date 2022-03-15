fad58d4faedf5b98614ce08e9bd8e6ff7ff7ce3a
MLRichter <matrichter@uos.de>
2022-03-15 10:18:10 +0100

    feat: changed linear layer handling behavior

--- phd_lab/models/efficentnet.py
+++ phd_lab/models/efficentnet.py
@@ -1,6 +1,7 @@
-from torchvision.models.efficientnet import efficientnet_b0
+from torchvision.models.efficientnet import efficientnet_b0 as eff_b0
 
-def efficentnet_b0(*args, **kwargs):
-    model = efficientnet_b0()
-    model.name = "EfficentNet_B0"
+
+def efficientnet_b0(*args, **kwargs):
+    model = eff_b0(**kwargs)
+    model.name = "EfficentNetB0"
     return model
--- phd_lab/optimizers/factories.py
+++ phd_lab/optimizers/factories.py
@@ -22,7 +22,30 @@ def lrs(model: Module,
         weight_decay: float = 5e-4,
         ) -> OptimizerSchedulerBundle:
     optimizer = SGD(model.parameters(), lr=0.1, momentum=0, weight_decay=0)
-    return OptimizerSchedulerBundle(optimizer=optimizer)
+    scheduler = StepLR(optimizer, step_size=10)
+    return OptimizerSchedulerBundle(optimizer=optimizer, scheduler=scheduler)
+
+
+def lrs_imgnet(model: Module,
+        lr: float = 0.01,
+        momentum: float = 0.9,
+        weight_decay: float = 5e-4,
+        ) -> OptimizerSchedulerBundle:
+    optimizer = SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+    scheduler = StepLR(optimizer, step_size=10)
+    return OptimizerSchedulerBundle(optimizer=optimizer, scheduler=scheduler)
+
+
+
+def lrs60_imgnet(model: Module,
+        lr: float = 0.01,
+        momentum: float = 0.9,
+        weight_decay: float = 5e-4,
+        ) -> OptimizerSchedulerBundle:
+    optimizer = SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+    scheduler = StepLR(optimizer, step_size=20)
+    return OptimizerSchedulerBundle(optimizer=optimizer, scheduler=scheduler)
+
 
 
 def lrs60(model: Module,
